62279df9b6463ecd23ee6bca8f86c017171667e3
ijinsam-lee <ijinsam@gmail.com>
2026-05-28 18:46:33 +0900

    2026app.py

--- 2026app.py
+++ 2026app.py
@@ -121,7 +121,7 @@ OFFENSIVE_C = ["FDN", "LIT", "SMH", "XLE"]  # 4대 주도 섹터
 DEFENSIVE_C = ["GLD", "PDBC", "OILK"]       # 3대 원자재 방어자산
 
 # 중복 없는 전체 티커 추출 (미국 ETF 랭킹 비교용 인기 자산군 SCHD, JEPI, TQQQ, SOXL, DIA, IWM, XLF 추가)
-ALL_TICKERS = list(set(["TIP", "SPY"] + OFFENSIVE_A + DEFENSIVE_A + OFFENSIVE_B + DEFENSIVE_B + OFFENSIVE_C + DEFENSIVE_C + ["SCHD", "JEPI", "TQQQ", "SOXL", "DIA", "IWM", "XLF"]))
+ALL_TICKERS = list(set(["TIP", "SPY"] + OFFENSIVE_A + DEFENSIVE_A + OFFENSIVE_B + DEFENSIVE_B + OFFENSIVE_C + DEFENSIVE_C + ["SOXX", "URA", "NLR","EWY", "SCHD", "JEPI", "TQQQ", "SOXL", "DIA", "IWM", "XLF"]))
 
 # 관심매크로 지표 정의 및 심볼 매핑
 MACRO_TICKERS = {
